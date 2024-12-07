396280e1e55dc442203b63fb047a18a77905d0b7
santiagodelpercioR <santiagodelpercio@gmail.com>
2024-12-07 16:33:43 -0300

    disenio responsive para pantallas chicas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./src/index.html"],
+  content: ["./index.html"],
   theme: {
     extend: {
       fontFamily: {
